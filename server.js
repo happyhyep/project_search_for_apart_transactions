323ce521ba1d4e671906524174fbcff0a25f93a2
SuminSSon <itoodo12@gmail.com>
2022-06-09 00:11:05 +0900

    Update port number

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.use(bodyParser.urlencoded({ extends: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use('/', route); // http://localhost:8080/ 이 주소로 들어갔을 때 app.js를 실행시켜라
+app.use('/', route);
 
-app.listen(8080, function (err) {
+app.listen(23023, function (err) {
   if (err) {
     console.log(err);
   } else {
-    console.log('listen:8080');
+    console.log('listen:23023');
   }
 });
